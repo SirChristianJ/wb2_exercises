1c48e323b69c2eec9c9fbdc39618892dfd0906fa
SirChristianJ <git@github.com:SirChristianJ/todoApp.git>
2024-10-02 12:09:52 -0400

    Initial commit

---
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        String firstName = promptUser("First Name: ");
+        String middleName = promptUser("Middle Name: ");
+        String lastName = promptUser("Last Name: ");
+        String suffix = promptUser("Suffix: ");
+        System.out.print("Full name: " + displayName(firstName,middleName,lastName,suffix));
+    }
+
+    public static String promptUser(String prompt){
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public static String displayName(String fName, String mName, String lName, String suffixName){
+        String fullName;
+        if(mName!=null && suffixName!=null){
+                fullName = fName + " " + mName + " " + lName + ", " + suffixName;
+
+        } else if (suffixName!=null) {
+                fullName = fName  + " " + lName + ", " + suffixName;
+
+        } else  fullName = fName + " " + mName + " " + lName;
+
+        return fullName.trim();
+    }
+
+}
